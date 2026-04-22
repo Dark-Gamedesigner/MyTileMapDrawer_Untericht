102a0d35a03a4ec66e833b61269a57aeef2e704a
André Boden <Feurigesdunkel29@gmail.com>
2026-04-22 09:37:16 +0200

    Add files via upload

---
+++ MyTileMapDrawer/MyTileMapDrawer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Raylib_cs;
+
+namespace MyTileMapDrawer;
+
+class Program
+{
+    static void Main(string[] args){
+        Console.WriteLine("Hello, World!");
+        Raylib.InitWindow(800, 600,"TileMaps! :D");
+        while (!Raylib.WindowShouldClose()){
+            Raylib.BeginDrawing();
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
